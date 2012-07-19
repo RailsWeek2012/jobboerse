12cc680c404bf333d86ff898f21a4988c5ed31c6
Torben Mehrmann <torben.mehrmann@mni.thm.de>
2012-07-19 15:03:59 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobBoerse::Application.config.secret_token = '75ee5d563f205bc93f76cf29551f7df003cdb437e3c305f25fc4183ebf26643b8a281e961e969db1a9daccdc3228fb747c9f6f5c43428f414d080acdcbea5e61'
---
+++ test/unit/helpers/jobs_controller_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class JobsControllerHelperTest < ActionView::TestCase
+end
