3430d8392ce35e074d1229ae0e5d10292eba303d
Torben Mehrmann <torben.mehrmann@mni.thm.de>
2012-07-24 22:36:03 +0200

    Einige Beschreibungen von Buttons / Taps geändert

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,7 +1,17 @@
 class PapersController < ApplicationController
+
   def index
     @paper = Paper.new(:name => params[:name])
   end
 
+  # def create
+#    @paper = Paper.new(params[:paper])
+ #   if @paper.save
+#      flash[:notice] = "Successfully created gallery."
+ #     redirect_to
+ #   else
+ #     render :action => 'new'
+ #   end
+  #end
 
 end
