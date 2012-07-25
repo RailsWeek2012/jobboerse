9fc2e24817cb420f8863a3f04c959306fc8d157d
Torben Mehrmann <torben.mehrmann@mni.thm.de>
2012-07-25 20:29:04 +0200

    Validierung in Tender angefangen

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -1,4 +1,8 @@
 class Tender < ActiveRecord::Base
   belongs_to(:user)
   attr_accessible :company_name, :contact, :description, :phone, :sector, :subject, :user_id
+  validates_presence_of :company_name, :contact, :description, :phone, :sector, :subject
+  validates_numericality_of :phone ,:only_integer => true , :message => "Bitte gueltige Telefonnummer angeben"
+
+
 end
