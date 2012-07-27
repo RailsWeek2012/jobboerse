9c63b84fa3de6044e4130ed5ecaf7cde88c6a32d
Sebastian F <sebastian.felsing@mni.thm.de>
2012-07-27 20:28:43 +0200

    last commit 2

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,7 +4,7 @@ class ApplicationsController < ApplicationController
   # GET /applications
   # GET /applications.json
   def index
-    @applications = current_user.application
+   @applications = current_user.applications
 
 
     respond_to do |format|
